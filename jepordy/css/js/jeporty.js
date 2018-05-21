e3f5b0c994d371af407468d1927e62f56a4446f7
kennf136 <kennf136@gmail.com>
2018-05-21 17:08:52 -0400

    end of day one

---
+++ jepordy/css/js/jeporty.js
@@ -0,0 +1,16 @@
+const Jeporty={ 
+    catigories: function (){
+         var topics=[ 'games', 'trivia', 'matale arts', 'movies', 'it', 'js', 'css','word problems']
+         var topicsPicked=[]
+         for (var i=0; i<5; i++){
+            if (math.random(0,10)==i ){
+                topicsPicked.push(topics[i])
+            }
+         }
+
+         function catigoryPossition(){
+             for ()
+
+         }
+}
+    }
