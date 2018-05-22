81c99aa58d1cddfa7a6d8ec47c4bc70fc7b12df0
kennf136 <kennf136@gmail.com>
2018-05-22 19:39:30 -0400

    im trying jamiegit add -A

--- jepordy/css/js/jeporty.js
+++ jepordy/css/js/jeporty.js
@@ -1,6 +1,6 @@
 
 
-    var modal = document.getElementById("modalback");
+var modal = document.getElementById("modalback");
 // this is what they pay the big money for
 var btn1 = document.getElementById("questions1-1");
 var btn2 = document.getElementById("questions1-2");
@@ -39,221 +39,221 @@ var btn34 = document.getElementById("questions6-4");
 var btn35 = document.getElementById("questions6-5");
 var btn36 = document.getElementById("questions6-6");
 
-
+var answer= ''
 var close = $("button .close");
 
-btn1.onclick = function() {
-    $('#modalback').css('display' , 'grid');
-    $('#modal-con1-1').css('display' , 'grid');
+btn1.onclick = function () {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con1-1').css('display', 'grid');
     $('.points1').hide()
 
 }
-btn2.onclick = function() {
+btn2.onclick = function () {
     console.log("does this function work?")
-    $('#modalback').css('display' , 'grid');
-    $('#modal-con1-2').css('display' , 'grid');
+    $('#modalback').css('display', 'grid');
+    $('#modal-con1-2').css('display', 'grid');
     $('.points2').hide()
 
 }
-btn3.onclick = function() {
-    $('#modalback').css('display' , 'grid');
-    $('#modal-con1-3').css('display' ,'grid');
+btn3.onclick = function () {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con1-3').css('display', 'grid');
     $('.points3').hide()
 
 }
-btn4.onclick = function() {
-    $('#modalback').css('display' , 'grid');
-    $('#modal-con1-4').css('display' , 'grid');
+btn4.onclick = function () {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con1-4').css('display', 'grid');
     $('.points4').hide()
 
 }
-btn5.onclick = function() {
-    $('#modalback').css('display' , 'grid');
-    $('#modal-con1-5').css('display' , 'grid');
+btn5.onclick = function () {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con1-5').css('display', 'grid');
     $('.points5').hide()
 
 }
-btn6.onclick = function() {
+btn6.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con1-6').css('display', 'grid');
     $('.points6').hide()
 
 }
-btn7.onclick = function() {
+btn7.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-1').css('display', 'grid');
     $('.points7').hide()
 
 }
-btn8.onclick = function() {
+btn8.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-2').css('display', 'grid');
     $('.points8').hide()
 
 }
-btn9.onclick = function() {
+btn9.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-3').css('display', 'grid');
     $('.points9').hide()
 
 }
-btn10.onclick = function() {
+btn10.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-4').css('display', 'grid');
     $('.points10').hide()
 
 }
-btn11.onclick = function() {
+btn11.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-5').css('display', 'grid');
     $('.points11').hide()
 
 }
-btn12.onclick = function() {
+btn12.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-6').css('display', 'grid');
     $('.points12').hide()
 
 }
-btn13.onclick = function() {
+btn13.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-1').css('display', 'grid');
     $('.points13').hide()
 
 }
-btn14.onclick = function() {
+btn14.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-2').css('display', 'grid');
     $('.points14').hide()
 
 }
-btn15.onclick = function() {
+btn15.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-3').css('display', 'grid');
     $('.points15').hide()
 
 }
-btn16.onclick = function() {
+btn16.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-4').css('display', 'grid');
     $('.points16').hide()
 
 }
-btn17.onclick = function() {
+btn17.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-5').css('display', 'grid');
     $('.points17').hide()
 
 }
-btn18.onclick = function() {
+btn18.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-6').css('display', 'grid');
     $('.points18').hide()
 
 }
-btn19.onclick = function() {
+btn19.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-1').css('display', 'grid');
     $('.points19').hide()
 
 }
-btn20.onclick = function() {
+btn20.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-2').css('display', 'grid');
     $('.points20').hide()
 
 }
-btn21.onclick = function() {
+btn21.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-3').css('display', 'grid');
     $('.points21').hide()
 
 }
-btn22.onclick = function() {
+btn22.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-4').css('display', 'grid');
     $('.points22').hide()
 
 }
-btn23.onclick = function() {
+btn23.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-5').css('display', 'grid');
     $('.points23').hide()
 
 }
-btn24.onclick = function() {
+btn24.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-6').css('display', 'grid');
     $('.points24').hide()
 
 }
-btn25.onclick = function() {
+btn25.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-1').css('display', 'grid');
     $('.points25').hide()
 
 }
-btn26.onclick = function() {
+btn26.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-2').css('display', 'grid');
     $('.points26').hide()
 
 }
-btn27.onclick = function() {
+btn27.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-3').css('display', 'grid');
     $('.points27').hide()
 
 }
-btn28.onclick = function() {
+btn28.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-4').css('display', 'grid');
     $('.points28').hide()
 
 }
-btn29.onclick = function() {
+btn29.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-5').css('display', 'grid');
     $('.points29').hide()
 
 }
-btn30.onclick = function() {
+btn30.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-6').css('display', 'grid');
     $('.points30').hide()
 
 }
-btn31.onclick = function() {
+btn31.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-1').css('display', 'grid');
     $('.points31').hide()
 
 }
-btn32.onclick = function() {
+btn32.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-2').css('display', 'grid');
     $('.points32').hide()
 
 }
-btn33.onclick = function() {
+btn33.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-3').css('display', 'grid');
     $('.points33').hide()
 
 }
-btn34.onclick = function() {
+btn34.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-4').css('display', 'grid');
     $('.points34').hide()
 
 }
-btn35.onclick = function() {
+btn35.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-5').css('display', 'grid');
     $('.points35').hide()
 
 }
-btn36.onclick = function() {
+btn36.onclick = function () {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-6').css('display', 'grid');
     $('.points36').hide()
@@ -267,9 +267,18 @@ btn36.onclick = function() {
 // }
 // same as
 
-$('.close'  ).click(function() {
+$('.close').click(function () {
     console.log("i am alive!!")
     $('#modalback').hide()
     $('.m').hide()
+    var answer=''
 
 })
+
+$('.g').click(function(){ answer=true; })
+$('.d').click(function(){answer=false;})
+
+if (answer=true){
+    console.log('Yes');
+}
+else if(answer=false){console.log('No')}
