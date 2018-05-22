22c0ea5d8a6e8f628c9e0ed1056527436da6d2a3
kennf136 <kennf136@gmail.com>
2018-05-22 15:02:21 -0400

    what am i getting paid for?

--- jepordy/css/js/jeporty.js
+++ jepordy/css/js/jeporty.js
@@ -21,6 +21,24 @@ var btn16 = document.getElementById("questions3-4");
 var btn17 = document.getElementById("questions3-5");
 var btn18 = document.getElementById("questions3-6");
 var btn19 = document.getElementById("questions4-1");
+var btn20 = document.getElementById("questions4-2");
+var btn21 = document.getElementById("questions4-3");
+var btn22 = document.getElementById("questions4-4");
+var btn23 = document.getElementById("questions4-5");
+var btn24 = document.getElementById("questions4-6");
+var btn25 = document.getElementById("questions5-1");
+var btn26 = document.getElementById("questions5-2");
+var btn27 = document.getElementById("questions5-3");
+var btn28 = document.getElementById("questions5-4");
+var btn29 = document.getElementById("questions5-5");
+var btn30 = document.getElementById("questions5-6");
+var btn31 = document.getElementById("questions6-1");
+var btn32 = document.getElementById("questions6-2");
+var btn33 = document.getElementById("questions6-3");
+var btn34 = document.getElementById("questions6-4");
+var btn35 = document.getElementById("questions6-5");
+var btn36 = document.getElementById("questions6-6");
+
 
 var close = $("button .close");
 
@@ -73,6 +91,104 @@ btn12.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-6').css('display', 'grid');
 }
+btn13.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con3-1').css('display', 'grid');
+}
+btn14.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con3-2').css('display', 'grid');
+}
+btn15.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con3-3').css('display', 'grid');
+}
+btn16.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con3-4').css('display', 'grid');
+}
+btn17.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con3-5').css('display', 'grid');
+}
+btn18.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con3-6').css('display', 'grid');
+}
+btn19.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con4-1').css('display', 'grid');
+}
+btn20.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con4-2').css('display', 'grid');
+}
+btn21.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con4-3').css('display', 'grid');
+}
+btn22.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con4-4').css('display', 'grid');
+}
+btn23.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con4-5').css('display', 'grid');
+}
+btn24.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con4-6').css('display', 'grid');
+}
+btn25.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con5-1').css('display', 'grid');
+}
+btn26.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con5-2').css('display', 'grid');
+}
+btn27.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con5-3').css('display', 'grid');
+}
+btn28.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con5-4').css('display', 'grid');
+}
+btn29.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con5-5').css('display', 'grid');
+}
+btn30.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con5-6').css('display', 'grid');
+}
+btn31.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con6-1').css('display', 'grid');
+}
+btn32.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con6-2').css('display', 'grid');
+}
+btn33.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con6-3').css('display', 'grid');
+}
+btn34.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con6-4').css('display', 'grid');
+}
+btn35.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con6-5').css('display', 'grid');
+}
+btn36.onclick = function() {
+    $('#modalback').css('display', 'grid');
+    $('#modal-con6-6').css('display', 'grid');
+}
+
+
 
 // getElementsByClassName("close").onclick = function() {
 //     console.log("i am alive!!")
