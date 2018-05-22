866380e1dd66a7978a659233f867a94b7df12c4e
kennf136 <kennf136@gmail.com>
2018-05-22 16:36:11 -0400

    added color and started thinking about my function for points

--- jepordy/css/js/jeporty.js
+++ jepordy/css/js/jeporty.js
@@ -45,147 +45,218 @@ var close = $("button .close");
 btn1.onclick = function() {
     $('#modalback').css('display' , 'grid');
     $('#modal-con1-1').css('display' , 'grid');
+    $('.points1').hide()
+
 }
 btn2.onclick = function() {
     console.log("does this function work?")
     $('#modalback').css('display' , 'grid');
     $('#modal-con1-2').css('display' , 'grid');
+    $('.points2').hide()
+
 }
 btn3.onclick = function() {
     $('#modalback').css('display' , 'grid');
     $('#modal-con1-3').css('display' ,'grid');
+    $('.points3').hide()
+
 }
 btn4.onclick = function() {
     $('#modalback').css('display' , 'grid');
     $('#modal-con1-4').css('display' , 'grid');
+    $('.points4').hide()
+
 }
 btn5.onclick = function() {
     $('#modalback').css('display' , 'grid');
     $('#modal-con1-5').css('display' , 'grid');
+    $('.points5').hide()
+
 }
 btn6.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con1-6').css('display', 'grid');
+    $('.points6').hide()
+
 }
 btn7.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-1').css('display', 'grid');
+    $('.points7').hide()
+
 }
 btn8.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-2').css('display', 'grid');
+    $('.points8').hide()
+
 }
 btn9.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-3').css('display', 'grid');
+    $('.points9').hide()
+
 }
 btn10.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-4').css('display', 'grid');
+    $('.points10').hide()
+
 }
 btn11.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-5').css('display', 'grid');
+    $('.points11').hide()
+
 }
 btn12.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con2-6').css('display', 'grid');
+    $('.points12').hide()
+
 }
 btn13.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-1').css('display', 'grid');
+    $('.points13').hide()
+
 }
 btn14.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-2').css('display', 'grid');
+    $('.points14').hide()
+
 }
 btn15.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-3').css('display', 'grid');
+    $('.points15').hide()
+
 }
 btn16.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-4').css('display', 'grid');
+    $('.points16').hide()
+
 }
 btn17.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-5').css('display', 'grid');
+    $('.points17').hide()
+
 }
 btn18.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con3-6').css('display', 'grid');
+    $('.points18').hide()
+
 }
 btn19.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-1').css('display', 'grid');
+    $('.points19').hide()
+
 }
 btn20.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-2').css('display', 'grid');
+    $('.points20').hide()
+
 }
 btn21.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-3').css('display', 'grid');
+    $('.points21').hide()
+
 }
 btn22.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-4').css('display', 'grid');
+    $('.points22').hide()
+
 }
 btn23.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-5').css('display', 'grid');
+    $('.points23').hide()
+
 }
 btn24.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con4-6').css('display', 'grid');
+    $('.points24').hide()
+
 }
 btn25.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-1').css('display', 'grid');
+    $('.points25').hide()
+
 }
 btn26.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-2').css('display', 'grid');
+    $('.points26').hide()
+
 }
 btn27.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-3').css('display', 'grid');
+    $('.points27').hide()
+
 }
 btn28.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-4').css('display', 'grid');
+    $('.points28').hide()
+
 }
 btn29.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-5').css('display', 'grid');
+    $('.points29').hide()
+
 }
 btn30.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con5-6').css('display', 'grid');
+    $('.points30').hide()
+
 }
 btn31.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-1').css('display', 'grid');
+    $('.points31').hide()
+
 }
 btn32.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-2').css('display', 'grid');
+    $('.points32').hide()
+
 }
 btn33.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-3').css('display', 'grid');
+    $('.points33').hide()
+
 }
 btn34.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-4').css('display', 'grid');
+    $('.points34').hide()
+
 }
 btn35.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-5').css('display', 'grid');
+    $('.points35').hide()
+
 }
 btn36.onclick = function() {
     $('#modalback').css('display', 'grid');
     $('#modal-con6-6').css('display', 'grid');
+    $('.points36').hide()
 }
 
 
